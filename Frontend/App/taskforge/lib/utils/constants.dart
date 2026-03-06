4a861f02dd77913e60065ca76c1247af868586f8
Ishfaq126 <79520440+Ishfaq126@users.noreply.github.com>
2026-03-06 16:52:51 +0500

    Update constants.dart

--- Frontend/App/taskforge/lib/utils/constants.dart
+++ Frontend/App/taskforge/lib/utils/constants.dart
@@ -1,9 +1,4 @@
 class ApiConstants {
-  // Mobile testing - Use laptop WiFi IP
-  static const String baseUrl = "http://10.72.125.97:4000/api"; // REST
-  static const String graphqlUrl = "http://10.72.125.97:4000/graphql"; // GraphQL
-  
-  // Local development URLs (uncomment when developing on same machine)
-  // static const String baseUrl = "http://localhost:4000/api"; // REST
-  // static const String graphqlUrl = "http://localhost:4000/graphql"; // GraphQL
+  static const String baseUrl = "https://tasks.digibizz.cloud/api";
+  static const String graphqlUrl = "https://tasks.digibizz.cloud/graphql";
 }
